0dbc45f7d38c5887feb592377ac1656e6568c90f
Tessilimi Theo <dafraise6@gmail.com>
2022-03-23 18:15:48 +0100

    make a little bite style on steamid page

--- src/Form/SteamIdType.php
+++ src/Form/SteamIdType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,12 @@ class SteamIdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('steamID')
-            ->add('submit', SubmitType::class)
+            ->add('steamID', TextType::class,[
+                'label' => 'Votre Steam ID :'
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'Mettre à jour'
+            ])
         ;
     }
 
