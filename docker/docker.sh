ef7b22ae09253088eb893c61d6c0214d9734dc77
Tessilimi Theo <dafraise6@gmail.com>
2022-03-27 13:59:49 +0200

    make the docker  for project change user name for postgres move database to postgressql

---
+++ docker/docker.sh
@@ -0,0 +1,3 @@
+php bin/console doctrine:database:create
+php bin/console d:m:m --no-interaction
+exec apache2-foreground
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: "`User`")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
