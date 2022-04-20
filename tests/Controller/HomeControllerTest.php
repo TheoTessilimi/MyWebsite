17d43c544a657bbbf40fb5f6aef1081d78510885
Tessilimi Theo <dafraise6@gmail.com>
2022-04-21 00:34:45 +0200

    Add more test

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -6,23 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $client = static::createClient();
-
-
-        $client->request('GET', '/');
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-
-        echo $client->getResponse()->getContent();
-    }
-
     /** @noinspection CssInvalidPseudoSelector */
     public function testHomePage(){
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur mon site !")')->count());
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 }
