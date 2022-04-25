cdf2f8d2b807b018bbaeaf865a534decb1b671de
Théo <Tessilimi.theo@gmail.com>
2022-04-25 19:41:30 +0200

    Add docker to the project

---
+++ Docker/deps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+composer install
+php bin/console doctrine:database:create
+php bin/console --no-interaction d:m:m
+npm install
+npm audit fix
+npm run dev
+echo "starting php-fpm"
+# shellcheck disable=SC2068
+exec $@
